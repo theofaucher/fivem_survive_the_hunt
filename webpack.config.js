c7a835532da9b30d04bf31d0c27d1e32b0ee4869
Théo Faucher <theofaucher44@gmail.com>
2019-07-05 12:17:01 +0200

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 module.exports = {
     entry: "./src/main.js",
+    entry: "./src/deathscreen/deathscreen.js",
     output: {
       filename: "init/cl_init.js",
       path: path.resolve(__dirname, 'resources')
