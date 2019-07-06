841d18619da3404e26329ec238e0ba1e9d83f4ad
Index_True <alix.biton@gmail.com>
2019-07-06 04:59:41 +0200

    Chased spawn system

--- src/sv_main.js
+++ src/sv_main.js
@@ -1,9 +1,9 @@
 /// <reference path="C:\Users\User\Desktop\FiveM_Hide_And_Seek\server-data\autocompletion\typings\index.d.ts" />
 import { huntersSpawnpoints } from './spawnpoints/hunters'
-import {getPlayers} from './utils/sv_players'
+import { chasedSpawnpoints } from './spawnpoints/chased'
+import { getPlayers } from './utils/sv_players'
 let chased;
 let hunters;
-console.log('STARTED')
 
 const huntersWeapons = [
     {
@@ -21,33 +21,39 @@ const huntersWeapons = [
 ]
 
 onNet('playerConnected', () => {
-    
+
     console.log(`${GetPlayerName(source)} connected!`)
 
     let location = huntersSpawnpoints[Math.floor(Math.random() * huntersSpawnpoints.length)]
     emitNet('spawn', source, location) // Force player respawn
     emitNet('giveWeapons', source, huntersWeapons)
 
-    emitNet('notify',-1,`~b~${GetPlayerName(source)}~s~ has connected`)
-    
+    emitNet('notify', -1, `~b~${GetPlayerName(source)}~s~ has connected`)
+
 
     if (GetNumPlayerIndices() >= GetConvarInt('min_players', 20)) {
         emit('startGame')
-    }else{
-        emitNet('notify',-1,`Game will start when ~b~${GetConvar('min_players')}~s~ players will be connected (~r~${GetConvar('min_players') - GetNumPlayerIndices()}~s~ to go)`)
+    } else {
+        emitNet('notify', -1, `Game will start when ~b~${GetConvar('min_players')}~s~ players will be connected (~r~${GetConvar('min_players') - GetNumPlayerIndices()}~s~ to go)`)
     }
 })
 
-on('startGame',()=>{
+on('startGame', () => {
     let players = getPlayers()
-    
     chased = players[Math.floor(Math.random() * players.length)]
-    hunters = players.slice(players.indexOf(chased),1)
+    players.slice(players.indexOf(chased), 1)
+    hunters = players
+
     console.log(`Game started, chased : ${GetPlayerName(chased)}`)
-    console.log(hunters)
-    emitNet('notify',chased,"The game started! you are ~r~chased~s~!~n~RUN!")
-    hunters.forEach(hunter=>{
-        emitNet('notify',hunter,`The game started! Kill ~r~${GetPlayerName(chased)}~s~ to win!`)
+
+    emitNet('spawn', chased, chasedSpawnpoints[Math.floor(Math.random() * chasedSpawnpoints.length)])
+    emitNet('giveWeapons',chased)
+    emitNet('notify', chased, "The game started! you are ~r~chased~s~!~n~RUN!")
+
+
+
+    hunters.forEach(hunter => {
+        emitNet('notify', hunter, `The game started! Kill ~r~${GetPlayerName(chased)}~s~ to win!`)
     })
 
 })
