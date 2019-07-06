a2e44e8da13746215f7f85c5fd364a9375ec89b3
Index_True <alix.biton@gmail.com>
2019-07-06 02:06:36 +0200

    initial spawn and weapon give now requested by the server

--- src/cl_main.js
+++ src/cl_main.js
@@ -1,16 +1,17 @@
 /// <reference path="C:\Users\User\Desktop\FiveM_Hide_And_Seek\server-data\autocompletion\typings\index.d.ts" />
 
 import * as spawnLib from './utils/spawn'
+import { WeaponsList } from './hashes/weapons'
 
-async function main (){
+async function main() {
     ShutdownLoadingScreen()
     await spawnLib.randomizePed(PlayerId())
-    await spawnLib.spawnPlayer(PlayerId(), {
+    /*await spawnLib.spawnPlayer(PlayerId(), {
         x: 0,
         y: 0,
         z: 71,
         heading: 0
-    })
+    })*/
     //Enable PvP
     NetworkSetFriendlyFireOption(true)
     SetCanAttackFriendly(PlayerPedId(), true, true)
@@ -19,10 +20,24 @@ async function main (){
 
 main()
 
-onNet('spawn', (location, enableTransition=false)=>{
-    if (enableTransition){
-        spawnLib.spawnPlayerWithTransition(PlayerId(),location)
-    }else{
-        spawnLib.spawnPlayer(PlayerId(),location)
+onNet('spawn', (location, enableTransition = false) => {
+    if (enableTransition) {
+        spawnLib.spawnPlayerWithTransition(PlayerId(), location)
+    } else {
+        spawnLib.spawnPlayer(PlayerId(), location)
+    }
+})
+
+onNet('giveWeapons', (weapons) => {
+    let ped = PlayerPedId()
+
+    if (typeof (weapons) == 'undefined') {
+        WeaponsList.forEach(element => {
+            GiveWeaponToPed(ped, element, 9999, false, false)
+        })
+    } else {
+        weapons.forEach(weapon => {
+            GiveWeaponToPed(ped, weapon.hash, weapon.ammo)
+        })
     }
 })
--- src/sv_main.js
+++ src/sv_main.js
@@ -1,18 +1,22 @@
 /// <reference path="C:\Users\User\Desktop\FiveM_Hide_And_Seek\server-data\autocompletion\typings\index.d.ts" />
-
-
-on('playerConnecting', (playerName) => {
-    console.log(`${playerName} is connecting`)
-})
+import {huntersSpawnpoints} from './spawnpoints/hunters'
 
 onNet('playerConnected', () => {
     console.log(`${GetPlayerName(source)} connected!`)
 
+    let location = huntersSpawnpoints[Math.floor(Math.random() * huntersSpawnpoints.length)]
+    emitNet('spawn',source,location) // Force player respawn
+    emitNet('giveWeapons',source,[
+        {
+            hash:'WEAPON_PUMPSHOTGUN',
+            ammo: 3*8
+        },
+        {
+            hash:'WEAPON_PISTOL',
+            ammo: 3*12
+        }
+    ])
     if (GetNumPlayerIndices() > GetConvarInt('min_players', 20)) {
         emit('startGame')
     }
-})
-
-on('playerDropped', (reason) => {
-    console.log(`${GetPlayerName(source)} disconnected : ${reason}`)
 })
