9500a858f8d21e32142aebb1917b915b0ffe8de0
Index_True <alix.biton@gmail.com>
2019-07-06 04:37:38 +0200

    Game start condition created

--- src/sv_main.js
+++ src/sv_main.js
@@ -1,22 +1,53 @@
 /// <reference path="C:\Users\User\Desktop\FiveM_Hide_And_Seek\server-data\autocompletion\typings\index.d.ts" />
-import {huntersSpawnpoints} from './spawnpoints/hunters'
+import { huntersSpawnpoints } from './spawnpoints/hunters'
+import {getPlayers} from './utils/sv_players'
+let chased;
+let hunters;
+console.log('STARTED')
+
+const huntersWeapons = [
+    {
+        hash: 'WEAPON_PUMPSHOTGUN',
+        ammo: 3 * 8
+    },
+    {
+        hash: 'WEAPON_PISTOL',
+        ammo: 3 * 12
+    },
+    {
+        hash: 'WEAPON_NIGHTSTICK',
+        ammo: 1
+    }
+]
 
 onNet('playerConnected', () => {
+    
     console.log(`${GetPlayerName(source)} connected!`)
 
     let location = huntersSpawnpoints[Math.floor(Math.random() * huntersSpawnpoints.length)]
-    emitNet('spawn',source,location) // Force player respawn
-    emitNet('giveWeapons',source,[
-        {
-            hash:'WEAPON_PUMPSHOTGUN',
-            ammo: 3*8
-        },
-        {
-            hash:'WEAPON_PISTOL',
-            ammo: 3*12
-        }
-    ])
-    if (GetNumPlayerIndices() > GetConvarInt('min_players', 20)) {
+    emitNet('spawn', source, location) // Force player respawn
+    emitNet('giveWeapons', source, huntersWeapons)
+
+    emitNet('notify',-1,`~b~${GetPlayerName(source)}~s~ has connected`)
+    
+
+    if (GetNumPlayerIndices() >= GetConvarInt('min_players', 20)) {
         emit('startGame')
+    }else{
+        emitNet('notify',-1,`Game will start when ~b~${GetConvar('min_players')}~s~ players will be connected (~r~${GetConvar('min_players') - GetNumPlayerIndices()}~s~ to go)`)
     }
+})
+
+on('startGame',()=>{
+    let players = getPlayers()
+    
+    chased = players[Math.floor(Math.random() * players.length)]
+    hunters = players.slice(players.indexOf(chased),1)
+    console.log(`Game started, chased : ${GetPlayerName(chased)}`)
+    console.log(hunters)
+    emitNet('notify',chased,"The game started! you are ~r~chased~s~!~n~RUN!")
+    hunters.forEach(hunter=>{
+        emitNet('notify',hunter,`The game started! Kill ~r~${GetPlayerName(chased)}~s~ to win!`)
+    })
+
 })
