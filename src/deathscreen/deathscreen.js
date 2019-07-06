88d29638918e5a2892f53ccab133068ebf9fc381
Index_True <alix.biton@gmail.com>
2019-07-06 14:50:58 +0200

    Removed debug prints

--- src/deathscreen/deathscreen.js
+++ src/deathscreen/deathscreen.js
@@ -4,9 +4,8 @@ import { Delay } from '../utils/wait'
 let locksound = false
 let tick
 
-on('events:playerDied', async() => {
+on('events:playerDied', async () => {
 
-        console.log("Un joueur est mort")
         StartScreenEffect("DeathFailOut", 0, 0)
 
         if (locksound == false) {
@@ -19,26 +18,25 @@ on('events:playerDied', async() => {
         ShakeGameplayCam("DEATH_FAIL_IN_EFFECT_SHAKE", 1.0)
         const scaleform = RequestScaleformMovie("MP_BIG_MESSAGE_FREEMODE")
 
-        while (!HasScaleformMovieLoaded(scaleform)) {await Delay(500)}
-                
-                tick = setTick(() => {
-                        PushScaleformMovieFunction(scaleform, "SHOW_SHARD_WASTED_MP_MESSAGE")
-                        BeginTextComponent("STRING")
-                        AddTextComponentString("~r~wasted")
-                        EndTextComponent()
-                        PopScaleformMovieFunctionVoid()
-                        DrawScaleformMovieFullscreen(scaleform, 255, 255, 255, 255)
-                })
+        while (!HasScaleformMovieLoaded(scaleform)) { await Delay(500) }
 
-                PlaySoundFrontend(-1, "TextHit", "WastedSounds", 1)
-    
+        tick = setTick(() => {
+                PushScaleformMovieFunction(scaleform, "SHOW_SHARD_WASTED_MP_MESSAGE")
+                BeginTextComponent("STRING")
+                AddTextComponentString("~r~wasted")
+                EndTextComponent()
+                PopScaleformMovieFunctionVoid()
+                DrawScaleformMovieFullscreen(scaleform, 255, 255, 255, 255)
+        })
+
+        PlaySoundFrontend(-1, "TextHit", "WastedSounds", 1)
+
 
 })
 
 
 on('events:playerAlive', () => {
 
-        console.log("Ca test")
         clearTick(tick);
         StopScreenEffect("DeathFailOut")
         locksound = false
