410f705433c2a12264e1eab8749a72d775aa17d4
Index_True <alix.biton@gmail.com>
2019-07-08 01:01:46 +0200

    Added player ids in list command

--- src/sv_commands.js
+++ src/sv_commands.js
@@ -3,7 +3,7 @@ import {getPlayers} from './utils/sv_players'
 RegisterCommand('list',()=>{
     console.log(`Connected players: ${GetNumPlayerIndices()}/${GetConvar('sv_maxClients')}`)
     getPlayers().forEach(player => {
-        console.log(GetPlayerName(player))
+        console.log(`${player} : ${GetPlayerName(player)}`)
     });
 })
 
