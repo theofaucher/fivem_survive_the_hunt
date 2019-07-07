1474aa346e5bab0673d3ec629f5ce2922b287145
Théo Faucher <theofaucher44@gmail.com>
2019-07-07 23:44:15 +0200

    Script redesign

--- src/wantedlevel/wantedlevel.js
+++ src/wantedlevel/wantedlevel.js
@@ -1,82 +1,108 @@
 /// <reference path="D:\Projets\FiveM\Survive_the_hunt\server-data\autocompletion\typings\index.d.ts" />
 
-//GetPlayerWantedLevel
-//ClearPlayerWantedLevel
-/*setInterval(function(){
+import './../events/death.js'
 
-   SetPlayerWantedLevel(PlayerId(), 4, 0)
-   SetPlayerWantedLevelNow(PlayerId(), 0)
-   console.log(GetPlayerWantedLevel())
-
-}*/
-import { Delay } from '../utils/wait'
 
 
 let startTime = 0
 let endTime = 0
+let dateSubtraction = 0
+let notificationContent
 var lockDateTo2Wanted = false
 var lockDateTo3Wanted = false
 
-setInterval(function(){
-    
-    if (GetPlayerWantedLevel() >= 2){
+setInterval(function () {
 
-        if (GetPlayerWantedLevel() == 2){
+    if (!IsPlayerDead()){
 
-            if ( lockDateTo2Wanted == false ){
-                startTime = Date.now()
-                endTime = 0
-                lockDateTo2Wanted = true
+        if (GetPlayerWantedLevel() >= 2) {
 
-            }
-    
-            endTime = Date.now()
+            if (GetPlayerWantedLevel() == 2) {
 
-                if (new Date(endTime - startTime).toISOString().substr(11, 8) == "00:01:00"){
-                    
+                if (lockDateTo2Wanted == false) {
+                    startTime = Date.now()
+                    endTime = 0
+                    lockDateTo2Wanted = true
+                    dateSubtraction = 0
+
+                }
+
+                endTime = Date.now()
+
+                dateSubtraction = Math.round((endTime - startTime) / 1000);
+
+                console.log(dateSubtraction)
+
+                notificationContent = `In maximum ${60 - dateSubtraction} second(s), you no longer have the corps`
+
+                if (dateSubtraction == 60) {
+
                     ClearPlayerWantedLevel()
                     lockDateTo2Wanted = false
                     lockDateTo3Wanted = false
                     startTime = 0
                     endTime = 0
+                    dateSubtraction = 0
 
                 }
-
-        }
-
-
-        if (GetPlayerWantedLevel() == 3){
-
-            if ( lockDateTo3Wanted == false ){
-
-                startTime = Date.now()
-                endTime = 0
-                lockDateTo3Wanted = true
 
             }
+
+
+            if (GetPlayerWantedLevel() == 3){
     
-            endTime = Date.now()
-
-                if (new Date(endTime - startTime).toISOString().substr(11, 8) == "00:00:30"){
-                    
-                    ClearPlayerWantedLevel()
-                    lockDateTo3Wanted = false
-                    lockDateTo2Wanted = false
-                    startTime = 0
+                if ( lockDateTo3Wanted == false ){
+    
+                    startTime = Date.now()
                     endTime = 0
-
+                    lockDateTo3Wanted = true
+                    dateSubtraction = 0
+    
                 }
+        
+                endTime = Date.now()
+    
+                dateSubtraction = Math.round((endTime - startTime)/1000);
+    
+                console.log(dateSubtraction)
+    
+                notificationContent = `In maximum ${30 - dateSubtraction} second(s), you no longer have the corps`
+    
+                    if (dateSubtraction == 30){
+                        
+                        ClearPlayerWantedLevel()
+                        lockDateTo2Wanted = false
+                        lockDateTo3Wanted = false
+                        startTime = 0
+                        endTime = 0
+                        dateSubtraction = 0
+    
+                    }
+    
+            }
 
         }
 
-        }
-
-        if (GetPlayerWantedLevel()>= 4){
+        if (GetPlayerWantedLevel() >= 4) {
 
             SetPlayerWantedLevel(PlayerId(), 3, 0)
             SetPlayerWantedLevelNow(PlayerId(), 0)
-    
+
         }
 
 
+    }
+
 }, 500)
+/*function drawTxt(content, font, colour, scale, x, y){
+    SetTextFont(font)
+    SetTextScale(scale, scale)
+    SetTextColour(colour[1],colour[2],colour[3], 255)
+    SetTextEntry("STRING")
+    SetTextDropShadow(0, 0, 0, 0,255)
+    SetTextDropShadow()
+    SetTextEdge(4, 0, 0, 0, 255)
+    SetTextOutline()
+    AddTextComponentString(content)
+    DrawText(x, y)
+}*/
